6eee0388c5999f9a1eb87309a33a78d77b80a7ee
Siam Rahman <siam@graduate.utm.my>
2025-01-21 09:02:22 +0800

    fix: security vulnerability

--- OnlineMarketplace/OnlineMarketplace/settings.py
+++ OnlineMarketplace/OnlineMarketplace/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_+%=5$x%3dt(76*sgqxgiod8+gq!)v8um8iveqt6^ckfdr@)fs"
+SECRET_KEY = [os.getenv('SECRET_KEY')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
